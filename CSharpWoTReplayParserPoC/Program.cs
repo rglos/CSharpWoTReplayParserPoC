c5a5c53e4f9b509019e689ddea14d0d783fb6d7f
Mark R. <mark+gh@mark.org.il>
2013-06-22 03:53:07 +0300

    Add rudimentary parsing & printing

--- CSharpWoTReplayParserPoC/Program.cs
+++ CSharpWoTReplayParserPoC/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CSharpWoTReplayParserPoC
 {
     class Program
     {
         static void Main(string[] args)
         {
+            WOTParser wp = new WOTParser();
+
+            if (args.Length > 0)
+            {
+                wp.processReplayFile(args[0]);
+            }
         }
     }
 }
---
+++ CSharpWoTReplayParserPoC/WOTParser.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronPython.Hosting;
+using IronPython.Runtime;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace CSharpWoTReplayParserPoC
+{
+    public class BytesArrayBinaryReader : BinaryReader
+    {
+        public BytesArrayBinaryReader(byte[] input)
+            : base(new MemoryStream(input))
+        {
+        }
+    }
+
+    class WOTParser
+    {
+        private ScriptEngine pyEngine = null;
+        private ScriptRuntime pyRuntime = null;
+        private ScriptScope pyScope = null;
+
+        private string extractDetailsBlob(string data)
+        {
+            Func<string, string> unpickle = pyScope.GetVariable<Func<string, string>>("load_details_blob");
+            return unpickle(data);
+        }
+
+        private void initPython()
+        {
+            if (pyEngine == null)
+            {
+                pyEngine = Python.CreateEngine();
+                pyScope = pyEngine.CreateScope();
+
+                ScriptSource src = pyEngine.CreateScriptSourceFromFile("scripts.py");
+
+                src.Execute(pyScope);
+            }
+        }
+
+        private dynamic runPythonCode(String code)
+        {
+            ScriptSource source = pyEngine.CreateScriptSourceFromString(code, SourceCodeKind.Statements);
+            CompiledCode compiled = source.Compile();
+            return compiled.Execute(pyScope);
+        }
+
+        public WOTParser()
+        {
+            initPython();
+        }
+
+        
+        public bool processReplayFile(string file)
+        {
+            byte[] ret = null;
+            try
+            {
+                FileStream fs = File.OpenRead(file);
+                BinaryReader br = new BinaryReader(fs);
+
+                fs.Seek(4, SeekOrigin.Begin); // Skip magic number
+                int blocks = br.ReadInt32();
+
+                if (blocks == 3)
+                {
+                    Console.WriteLine("[+] Loading chunks, {0} blocks found", blocks);
+
+                    blocks = br.ReadInt32();
+                    Console.WriteLine("[+] Players chunk length {0}", blocks);
+                    byte[] data = br.ReadBytes(blocks);
+                    
+                    // XXX: Player data
+                    Console.WriteLine(Encoding.UTF8.GetString(data, 0, data.Length));
+
+                    blocks = br.ReadInt32();
+                    Console.WriteLine("[+] Frags chunk length {0}", blocks);
+                    data = br.ReadBytes(blocks);
+
+                    // XXX: Frag data
+                    Console.WriteLine(Encoding.UTF8.GetString(data, 0, data.Length));
+
+                    blocks = br.ReadInt32();
+                    Console.WriteLine("[+] Details pickle length {0}", blocks);
+                    data = br.ReadBytes(blocks);
+
+                    // XXX: Details data
+                    Console.WriteLine(extractDetailsBlob(System.Text.Encoding.Default.GetString(data)));
+
+                }
+                else
+                {
+                    Console.WriteLine("[-] Insufficient block count");
+                }
+
+                br.Close();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
